a65a6555cdbbe7d11add4a5cbcbf9fb8af0ba3c5
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2020-03-31 19:56:14 +0300

    Add empty project
     - Kotlin/Multiplatform 1.3.71
     - Gradle 6.3
     - JUnit 5.6.1

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.multiplatform") version "1.3.71"
+}
+
+group = "org.buddy"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("junit:junit:5.6.1")
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        commonTest {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
